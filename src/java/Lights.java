1a96b13b82684280127371c9b72cebd190bed59a
Adam Watney <79660281+AdaWat@users.noreply.github.com>
2022-05-23 19:03:50 +0100

    Update Lights.java
    added gradients on lights

--- src/java/Lights.java
+++ src/java/Lights.java
@@ -4,29 +4,53 @@ import java.awt.*;
 
 public class Lights extends JComponent {
     private int state = 1;      // there are 4 states: (red), (red & amber), (green), (amber)
+    private float[] dist = {0.3f, 1.0f};
 
     @Override
     protected void paintComponent (Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);   // make circles look smooth
+
         Rectangle2D.Double back = new Rectangle2D.Double(50, 50, 100, 260);
-        g2d.setColor(new Color(10, 10, 10));
+        GradientPaint grad1 = new GradientPaint(50, 50, new Color(10, 10, 10), 150, 50, new Color(60, 60, 60));
+        g2d.setPaint(grad1);
         g2d.fill(back);
 
         Rectangle2D.Double pole = new Rectangle2D.Double(80, 310, 40, 90);
-        g2d.setColor(new Color(150, 150, 150));
+        GradientPaint grad2 = new GradientPaint(80, 310, new Color(50, 50, 50), 120, 310, new Color(150, 150, 150));
+        g2d.setPaint(grad2);
         g2d.fill(pole);
 
         Ellipse2D.Double c1 = new Ellipse2D.Double(70, 70, 60, 60);
-        if (state == 1 || state == 2) {g2d.setColor(Color.RED);} else {g2d.setColor(Color.DARK_GRAY);}
+        Color[] c1cols = {new Color(255, 150, 150), Color.RED};
+        if (state == 1 || state == 2) {
+            RadialGradientPaint g1 = new RadialGradientPaint(new Point2D.Float(100, 100), 30, dist, c1cols);
+            g2d.setPaint(g1);
+        } else {
+            g2d.setColor(Color.DARK_GRAY);
+        }
         g2d.fill(c1);
 
         Ellipse2D.Double c2 = new Ellipse2D.Double(70, 150, 60, 60);
-        if (state == 2 || state == 4) {g2d.setColor(Color.ORANGE);} else {g2d.setColor(Color.DARK_GRAY);}
+        Color[] c2cols = {new Color(255, 250, 120), Color.ORANGE};
+        if (state == 2 || state == 4) {
+            RadialGradientPaint g2 = new RadialGradientPaint(new Point2D.Float(100, 180), 30, dist, c2cols);
+            g2d.setPaint(g2);
+        } else {
+            g2d.setColor(Color.DARK_GRAY);
+        }
         g2d.fill(c2);
 
         Ellipse2D.Double c3 = new Ellipse2D.Double(70, 230, 60, 60);
-        if (state == 3) {g2d.setColor(Color.GREEN);} else {g2d.setColor(Color.DARK_GRAY);}
+        Color[] c3cols = {new Color(150, 255, 150), Color.GREEN};
+        if (state == 3) {
+            RadialGradientPaint g3 = new RadialGradientPaint(new Point2D.Float(100, 260), 30, dist, c3cols);
+            g2d.setPaint(g3);
+        } else {
+            g2d.setColor(Color.DARK_GRAY);
+        }
+
         g2d.fill(c3);
     }
 
